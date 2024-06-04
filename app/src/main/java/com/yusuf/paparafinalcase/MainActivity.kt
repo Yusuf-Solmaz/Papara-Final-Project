eaad1c352945fca323a34a94876b1954bb96de85
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-04 16:50:14 +0300

    UI Update

--- app/src/main/java/com/yusuf/paparafinalcase/MainActivity.kt
+++ app/src/main/java/com/yusuf/paparafinalcase/MainActivity.kt
@@ -11,37 +11,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.yusuf.paparafinalcase.presentation.foodScreen.FoodScreen
 import com.yusuf.paparafinalcase.ui.theme.PaparaFinalCaseTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PaparaFinalCaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                FoodScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PaparaFinalCaseTheme {
-        Greeting("Android")
+
     }
 }
