98eb38733e69c1b23a1d4aa88720dad52f962700
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-05 12:21:29 +0300

    Search Recipe Repository Completed

---
+++ app/src/main/java/com/yusuf/paparafinalcase/data/remote/repository/searchRecipeRepository/SearchRecipeRepository.kt
@@ -0,0 +1,11 @@
+package com.yusuf.paparafinalcase.data.remote.repository.searchRecipeRepository
+
+import com.yusuf.paparafinalcase.core.rootFlow.rootFlow
+import com.yusuf.paparafinalcase.data.remote.network.ApiService
+import javax.inject.Inject
+
+class SearchRecipeRepository @Inject constructor(val apiService: ApiService) {
+    fun searchRecipes(query: String, diet: String?, cuisine: String?) = rootFlow {
+        apiService.searchRecipes(query = query, diet = diet, cuisine = cuisine)
+    }
+}
