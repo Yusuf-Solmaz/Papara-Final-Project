469bb6f5feb95fa6210ff12591ff12d7400eb4c6
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-05 12:22:41 +0300

    Search Recipe Part Completed

--- app/src/main/java/com/yusuf/paparafinalcase/presentation/foodScreen/FoodScreen.kt
+++ app/src/main/java/com/yusuf/paparafinalcase/presentation/foodScreen/FoodScreen.kt
@@ -8,14 +8,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
@@ -27,16 +32,20 @@ import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.yusuf.paparafinalcase.R
 import com.yusuf.paparafinalcase.data.remote.responses.recipe.Recipe
+import com.yusuf.paparafinalcase.data.remote.responses.searchRecipe.Result
 import com.yusuf.paparafinalcase.presentation.components.LoadingLottie
 import com.yusuf.paparafinalcase.presentation.foodScreen.viewmodel.FoodScreenViewModel
 import com.yusuf.paparafinalcase.presentation.foodScreen.viewmodel.RandomFoodState
+import com.yusuf.paparafinalcase.presentation.foodScreen.viewmodel.SearchRecipeState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FoodScreen (viewModel: FoodScreenViewModel = hiltViewModel()) {
 
     val randomFoodState by viewModel.rootRandomFoodResponse.collectAsState(RandomFoodState())
+    val searchRecipeState by viewModel.rootSearchRecipeResponse.collectAsState(SearchRecipeState())
 
+    var searchQuery by remember { mutableStateOf("") }
 
     if (randomFoodState.error != null){
         Column(
@@ -72,28 +81,74 @@ fun FoodScreen (viewModel: FoodScreenViewModel = hiltViewModel()) {
                         .fillMaxSize()
                 ) {
 
+                    Row(modifier = Modifier.fillMaxWidth(),verticalAlignment = Alignment.CenterVertically) {
+                        OutlinedTextField(
+                            value = searchQuery,
+                            onValueChange = { newText ->
+                                searchQuery = newText
+                            },
+                            label = { Text("Enter querry") },
+                            placeholder = { Text("Placeholder") },
+                            modifier = Modifier
+                                .padding(vertical = 8.dp)
+                                .weight(3f)
+                        )
+
+                        Button(
+                            onClick = {
+                                viewModel.searchRecipe(searchQuery, null, null)
+                            }, modifier = Modifier.weight(1f)
+
+                        ) {
+                            Text(text = "Filter")
+                        }
+                    }
+
+
+
                     LazyColumn(
                         modifier = Modifier
                             .weight(1f)
                             .fillMaxWidth()
                     ) {
-                        randomFoodState.rootResponse?.recipes?.let {
-                            items(it.size) { index ->
-                                val randomRecipe = randomFoodState.rootResponse!!.recipes[index]
-                                RandomFoodItem(randomRecipe)
+                        val searchResults = searchRecipeState.rootResponse?.results
+                        if (!searchResults.isNullOrEmpty()) {
+                            items(searchResults.size) { index ->
+                                val searchRecipe = searchResults[index]
+                                SearchFoodItem(searchRecipe)
+                            }
+                        }
+                        else {
+                            val randomRecipes = randomFoodState.rootResponse?.recipes
+                            if (!randomRecipes.isNullOrEmpty()) {
+                                items(randomRecipes.size) { index ->
+                                    val randomRecipe = randomRecipes[index]
+                                    RandomFoodItem(randomRecipe)
+                                }
                             }
                         }
                     }
-
                 }
+            })
 
-            }
-        )
-    }
+       }
+
 }
+
+
 
 @Composable
 fun RandomFoodItem(randomFood: Recipe) {
+    LazyRowRecipeItem(image = randomFood.image, title = randomFood.title)
+}
+
+@Composable
+fun SearchFoodItem(searchFood: Result) {
+    LazyRowRecipeItem(image = searchFood.image, title = searchFood.title)
+}
+
+@Composable
+fun LazyRowRecipeItem(image:String, title:String){
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading_food_image_lottie))
 
     Card(
@@ -109,7 +164,7 @@ fun RandomFoodItem(randomFood: Recipe) {
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             SubcomposeAsyncImage(
-                model = randomFood.image,
+                model = image,
                 contentDescription = null,
                 modifier = Modifier.size(100.dp, 100.dp),
                 loading = {
@@ -122,7 +177,7 @@ fun RandomFoodItem(randomFood: Recipe) {
             )
 
             Text(
-                text = randomFood.title,
+                text = title,
                 maxLines = 2,
                 overflow = TextOverflow.Ellipsis,
                 modifier = Modifier.padding(start = 8.dp)
