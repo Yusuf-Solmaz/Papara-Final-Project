658d2bf466faaf1ffbf27e7e5850cf7e1d5f70e0
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-05 17:48:24 +0300

    Recipe Detail Screen Created

---
+++ app/src/main/java/com/yusuf/paparafinalcase/presentation/recipeDetailScreen/RecipeDetailScreen.kt
@@ -0,0 +1,17 @@
+package com.yusuf.paparafinalcase.presentation.recipeDetailScreen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun RecipeDetailScreen(navController: NavController, recipeId: Int) {
+    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Text(text = recipeId.toString())    
+    }
+}
