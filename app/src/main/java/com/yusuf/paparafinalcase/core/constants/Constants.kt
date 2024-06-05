a08e8ba5a65b97730e018c646047a5f6cecde2b3
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-05 17:46:51 +0300

    Categories Added

---
+++ app/src/main/java/com/yusuf/paparafinalcase/core/constants/Constants.kt
@@ -0,0 +1,22 @@
+package com.yusuf.paparafinalcase.core.constants
+
+import com.yusuf.paparafinalcase.R
+
+object Constants {
+    val categories = listOf(
+"African", "Asian", "American", "Caribbean", "Chinese", "European", "French", "German", "Indian", "Japanese", "Mexican")
+
+    val categoryImages = mapOf(
+        "African" to R.drawable.african,
+        "Asian" to R.drawable.asian,
+        "American" to R.drawable.american,
+        "Caribbean" to R.drawable.caribbean,
+        "Chinese" to R.drawable.chinese,
+        "European" to R.drawable.european,
+        "French" to R.drawable.french,
+        "German" to R.drawable.german,
+        "Indian" to R.drawable.indian,
+        "Japanese" to R.drawable.japanese,
+        "Mexican" to R.drawable.mexican
+    )
+}
