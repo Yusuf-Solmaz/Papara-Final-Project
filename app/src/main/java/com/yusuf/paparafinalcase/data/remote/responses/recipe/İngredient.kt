a9cdf9b37ba5e889e1a1373ea05270e05503ec61
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-08 00:22:44 +0300

    Update

--- app/src/main/java/com/yusuf/paparafinalcase/data/remote/responses/recipe/İngredient.kt
+++
@@ -1,8 +0,0 @@
-package com.yusuf.paparafinalcase.data.remote.responses.recipe
-
-data class İngredient(
-    val id: Int,
-    val image: String,
-    val localizedName: String,
-    val name: String
-)
