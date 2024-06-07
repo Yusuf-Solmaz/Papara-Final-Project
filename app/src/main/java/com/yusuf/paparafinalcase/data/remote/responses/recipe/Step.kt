151c59a15fb93e63ae741a818229d9e155488110
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-08 00:21:03 +0300

    Update

--- app/src/main/java/com/yusuf/paparafinalcase/data/remote/responses/recipe/Step.kt
+++ app/src/main/java/com/yusuf/paparafinalcase/data/remote/responses/recipe/Step.kt
@@ -2,7 +2,7 @@ package com.yusuf.paparafinalcase.data.remote.responses.recipe
 
 data class Step(
     val equipment: List<Equipment>,
-    val ingredients: List<İngredient>,
+    val ingredients: List<Ingredient>,
     val length: Length,
     val number: Int,
     val step: String
