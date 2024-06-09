3c667b11696fb3ebc4800f549f9c30d05eb6e5de
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-09 12:43:35 +0300

    Food Screen Updated

--- app/src/main/java/com/yusuf/paparafinalcase/presentation/foodScreen/FoodScreen.kt
+++ app/src/main/java/com/yusuf/paparafinalcase/presentation/foodScreen/FoodScreen.kt
@@ -3,6 +3,7 @@ package com.yusuf.paparafinalcase.presentation.foodScreen
 import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,30 +11,43 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import coil.compose.SubcomposeAsyncImage
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.rememberLottieComposition
 import com.yusuf.paparafinalcase.R
 import com.yusuf.paparafinalcase.data.remote.responses.recipe.Recipe
 import com.yusuf.paparafinalcase.data.remote.responses.searchRecipe.Result
@@ -42,21 +56,43 @@ import com.yusuf.paparafinalcase.presentation.components.LoadingLottie
 import com.yusuf.paparafinalcase.presentation.foodScreen.viewmodel.FoodScreenViewModel
 import com.yusuf.paparafinalcase.presentation.foodScreen.viewmodel.RandomFoodState
 import com.yusuf.paparafinalcase.presentation.foodScreen.viewmodel.SearchRecipeState
+import java.util.Locale
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FoodScreen (navController: NavController,category: String?,viewModel: FoodScreenViewModel = hiltViewModel()) {
+fun FoodScreen(navController: NavController, category: String?, viewModel: FoodScreenViewModel = hiltViewModel()) {
 
     val randomFoodState by viewModel.rootRandomFoodResponse.collectAsState(RandomFoodState())
     val searchRecipeState by viewModel.rootSearchRecipeResponse.collectAsState(SearchRecipeState())
 
+    val supportedCuisines = listOf(
+        "African", "Asian", "American", "British", "Cajun", "Caribbean", "Chinese",
+        "Eastern European", "European", "French", "German", "Greek", "Indian", "Irish",
+        "Italian", "Japanese", "Jewish", "Korean", "Latin American", "Mediterranean",
+        "Mexican", "Middle Eastern", "Nordic", "Southern", "Spanish", "Thai", "Vietnamese"
+    )
+
+    val supportedDiets = listOf(
+        "Gluten Free", "Ketogenic", "Vegetarian", "Lacto-Vegetarian", "Ovo-Vegetarian",
+        "Vegan", "Pescetarian", "Paleo", "Primal", "Low FODMAP", "Whole30"
+    )
+
+    var isCuisineDropdownOpen by remember { mutableStateOf(false) }
+    var isDietDropdownOpen by remember { mutableStateOf(false) }
+    var selectedCuisine by remember { mutableStateOf<String?>(null) }
+    var selectedDiet by remember { mutableStateOf<String?>(null) }
+
     var searchQuery by remember { mutableStateOf("") }
+
+    val sheetState = rememberModalBottomSheetState()
+    var isSheetOpen by rememberSaveable { mutableStateOf(false) }
 
     LaunchedEffect(key1 = true) {
         Log.i("Category", "FoodScreen: $category")
     }
 
-    if (randomFoodState.error != null){
+    if (randomFoodState.error != null) {
         Column(
             Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
@@ -66,7 +102,6 @@ fun FoodScreen (navController: NavController,category: String?,viewModel: FoodSc
         }
     }
 
-
     if (randomFoodState.isLoading) {
         Column(
             Modifier.fillMaxSize(),
@@ -75,8 +110,7 @@ fun FoodScreen (navController: NavController,category: String?,viewModel: FoodSc
         ) {
             LoadingLottie(R.raw.general_loading_lottie)
         }
-    }
-     else {
+    } else {
         Scaffold(
             topBar = {
                 TopAppBar(
@@ -89,14 +123,13 @@ fun FoodScreen (navController: NavController,category: String?,viewModel: FoodSc
                         .padding(paddingValues)
                         .fillMaxSize()
                 ) {
+                    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
 
-                    Row(modifier = Modifier.fillMaxWidth(),verticalAlignment = Alignment.CenterVertically) {
+
                         OutlinedTextField(
                             value = searchQuery,
-                            onValueChange = { newText ->
-                                searchQuery = newText
-                            },
-                            label = { Text("Enter querry") },
+                            onValueChange = { newText -> searchQuery = newText },
+                            label = { Text("Enter query") },
                             placeholder = { Text("Placeholder") },
                             modifier = Modifier
                                 .padding(vertical = 8.dp)
@@ -105,15 +138,13 @@ fun FoodScreen (navController: NavController,category: String?,viewModel: FoodSc
 
                         Button(
                             onClick = {
-                                viewModel.searchRecipe(searchQuery, null, null)
-                            }, modifier = Modifier.weight(1f)
-
+                                isSheetOpen = true
+                            },
+                            modifier = Modifier.weight(1f)
                         ) {
                             Text(text = "Filter")
                         }
                     }
-
-
 
                     LazyColumn(
                         modifier = Modifier
@@ -124,39 +155,146 @@ fun FoodScreen (navController: NavController,category: String?,viewModel: FoodSc
                         if (!searchResults.isNullOrEmpty()) {
                             items(searchResults.size) { index ->
                                 val searchRecipe = searchResults[index]
-                                SearchFoodItem(searchRecipe){
+                                SearchFoodItem(searchRecipe) {
                                     navController.navigate("recipe_detail_page/${searchRecipe.id}")
                                 }
                             }
-                        }
-                        else {
+                        } else {
                             val randomRecipes = randomFoodState.rootResponse?.recipes
                             if (!randomRecipes.isNullOrEmpty()) {
                                 items(randomRecipes.size) { index ->
                                     val randomRecipe = randomRecipes[index]
-                                    RandomFoodItem(randomRecipe){
+                                    RandomFoodItem(randomRecipe) {
                                         navController.navigate("recipe_detail_page/${randomRecipe.id}")
                                     }
                                 }
                             }
                         }
                     }
+
+                    if (isSheetOpen) {
+                        ModalBottomSheet(
+                            modifier = Modifier
+                                .padding(bottom = paddingValues.calculateBottomPadding())
+                                .size(300.dp, 300.dp),
+                            sheetState = sheetState,
+                            onDismissRequest = {
+                                isSheetOpen = false
+                            }
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .padding(16.dp),
+                                verticalArrangement = Arrangement.SpaceBetween,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceEvenly
+                                ) {
+                                    Box(modifier = Modifier.weight(1f)) {
+                                        Row(
+                                            verticalAlignment = Alignment.CenterVertically,
+                                            horizontalArrangement = Arrangement.Center,
+                                            modifier = Modifier
+                                                .clickable { isCuisineDropdownOpen = !isCuisineDropdownOpen }
+                                                .padding(16.dp)
+                                                .size(150.dp, 50.dp)
+                                        ) {
+                                            Text(text = selectedCuisine ?: "Select Cuisine")
+                                            Icon(
+                                                imageVector = Icons.Default.ArrowDropDown,
+                                                contentDescription = "Drop-Down Arrow",
+                                                tint = Color.Black
+                                            )
+                                        }
+
+                                        DropdownMenu(
+                                            expanded = isCuisineDropdownOpen,
+                                            onDismissRequest = { isCuisineDropdownOpen = false },
+                                            modifier = Modifier.size(150.dp, 300.dp)
+                                        ) {
+                                            supportedCuisines.forEach { cuisine ->
+                                                DropdownMenuItem(
+                                                    text = { Text(cuisine) },
+                                                    onClick = {
+                                                        selectedCuisine = cuisine
+                                                        isCuisineDropdownOpen = false
+                                                    }
+                                                )
+                                            }
+                                        }
+                                    }
+
+                                    Box(modifier = Modifier.weight(1f)) {
+                                        Row(
+                                            verticalAlignment = Alignment.CenterVertically,
+                                            horizontalArrangement = Arrangement.Center,
+                                            modifier = Modifier
+                                                .clickable { isDietDropdownOpen = !isDietDropdownOpen }
+                                                .padding(16.dp)
+                                                .size(150.dp, 50.dp)
+                                        ) {
+                                            Text(text = selectedDiet ?: "Select Diet")
+                                            Icon(
+                                                imageVector = Icons.Default.ArrowDropDown,
+                                                contentDescription = "Drop-Down Arrow",
+                                                tint = Color.Black
+                                            )
+                                        }
+
+                                        DropdownMenu(
+                                            expanded = isDietDropdownOpen,
+                                            onDismissRequest = { isDietDropdownOpen = false },
+                                            modifier = Modifier.size(150.dp, 300.dp)
+                                        ) {
+                                            supportedDiets.forEach { diet ->
+                                                DropdownMenuItem(
+                                                    text = { Text(diet) },
+                                                    onClick = {
+                                                        selectedDiet = diet
+                                                        isDietDropdownOpen = false
+                                                    }
+                                                )
+                                            }
+                                        }
+                                    }
+                                }
+
+                                Button(
+                                    onClick = {
+                                        viewModel.searchRecipe(searchQuery,
+                                            selectedCuisine?.lowercase(Locale.getDefault()), selectedDiet)
+                                        isSheetOpen = false
+                                    },
+                                    modifier = Modifier
+                                        .align(Alignment.CenterHorizontally)
+                                        .padding(16.dp)
+                                ) {
+                                    Text(text = "Filter")
+                                }
+                            }
+                        }
+                    }
                 }
-            })
-
-       }
-
+            }
+        )
+    }
 }
+
+/*
+*
+                       */
 
 
 
 @Composable
-fun RandomFoodItem(randomFood: Recipe,onCardClick: () ->Unit) {
-    LazyColumnRecipeItem(image = randomFood.image, title = randomFood.title,onCardClick)
+fun RandomFoodItem(randomFood: Recipe, onCardClick: () -> Unit) {
+    LazyColumnRecipeItem(image = randomFood.image, title = randomFood.title, onCardClick)
 }
 
 @Composable
-fun SearchFoodItem(searchFood: Result,onCardClick: () ->Unit) {
-    LazyColumnRecipeItem(image = searchFood.image, title = searchFood.title,onCardClick)
+fun SearchFoodItem(searchFood: Result, onCardClick: () -> Unit) {
+    LazyColumnRecipeItem(image = searchFood.image, title = searchFood.title, onCardClick)
 }
-
