5bac4226a5309536a2eb418cfd6bce4bd918d460
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-10 12:17:22 +0300

    LocalFoods.toRecipeInfoRoot() Added

--- app/src/main/java/com/yusuf/paparafinalcase/data/mapper/MapExtension.kt
+++ app/src/main/java/com/yusuf/paparafinalcase/data/mapper/MapExtension.kt
@@ -1,7 +1,21 @@
 package com.yusuf.paparafinalcase.data.mapper
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.yusuf.paparafinalcase.data.local.model.LocalFoods
+import com.yusuf.paparafinalcase.data.remote.responses.recipe.ExtendedIngredient
 import com.yusuf.paparafinalcase.data.remote.responses.recipe.RecipeInfoRoot
+
+fun List<ExtendedIngredient>.toJsonString(): String {
+    val gson = Gson()
+    return gson.toJson(this)
+}
+
+fun String.toIngredientsList(): List<ExtendedIngredient> {
+    val gson = Gson()
+    val type = object : TypeToken<List<ExtendedIngredient>>() {}.type
+    return gson.fromJson(this, type)
+}
 
 fun RecipeInfoRoot.toLocalFoods(): LocalFoods{
     return LocalFoods(
@@ -15,6 +29,50 @@ fun RecipeInfoRoot.toLocalFoods(): LocalFoods{
         cookingMinutes = this.cookingMinutes,
         servings = this.servings,
         aggregateLikes = this.aggregateLikes,
-        instructions = this.instructions
+        instructions = this.instructions,
+        ingredients = this.extendedIngredients.toJsonString()
+
+    )
+}
+
+fun LocalFoods.toRecipeInfoRoot(): RecipeInfoRoot {
+
+    return RecipeInfoRoot(
+        aggregateLikes = this.aggregateLikes,
+        analyzedInstructions = listOf(),
+        cheap = false,
+        cookingMinutes = this.cookingMinutes,
+        creditsText = "",
+        cuisines = listOf(),
+        dairyFree = false,
+        diets = listOf(),
+        dishTypes = listOf(),
+        extendedIngredients = this.ingredients.toIngredientsList(),
+        gaps = "",
+        glutenFree = this.glutenFree,
+        healthScore = 0,
+        id = this.foodId,
+        image = this.image,
+        imageType = "",
+        instructions = this.instructions,
+        lowFodmap = false,
+        occasions = listOf(),
+        originalId = null,
+        preparationMinutes = this.preparationMinutes,
+        pricePerServing = 0.0,
+        readyInMinutes = this.preparationMinutes + this.cookingMinutes,
+        servings = this.servings,
+        sourceName = "",
+        sourceUrl = "",
+        spoonacularScore = 0.0,
+        spoonacularSourceUrl = "",
+        summary = "",
+        sustainable = false,
+        title = this.title,
+        vegan = false,
+        vegetarian = this.vegetarian,
+        veryHealthy = this.veryHealthy,
+        veryPopular = false,
+        weightWatcherSmartPoints = 0
     )
 }
