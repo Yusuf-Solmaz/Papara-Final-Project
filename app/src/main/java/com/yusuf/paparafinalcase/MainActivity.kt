4289fa438c44e5ce6c6d0b9daa6b6f6444b6d69b
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-10 12:19:51 +0300

    MainViewModel Added to Navigation

--- app/src/main/java/com/yusuf/paparafinalcase/MainActivity.kt
+++ app/src/main/java/com/yusuf/paparafinalcase/MainActivity.kt
@@ -4,20 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.yusuf.paparafinalcase.navigation.Navigation
+import com.yusuf.paparafinalcase.presentation.mainViewmodel.MainViewModel
 import com.yusuf.paparafinalcase.ui.theme.PaparaFinalCaseTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PaparaFinalCaseTheme {
-                Navigation()
+                Navigation(mainViewModel = mainViewModel)
             }
         }
     }
