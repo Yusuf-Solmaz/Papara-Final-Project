840335f8b014766a25a3d55e45ddc927e340e6a4
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-12 02:12:43 +0300

    Splash Screen Update

--- app/src/main/java/com/yusuf/paparafinalcase/presentation/splashScreen/SplashSceen.kt
+++ app/src/main/java/com/yusuf/paparafinalcase/presentation/splashScreen/SplashSceen.kt
@@ -1,20 +1,66 @@
 package com.yusuf.paparafinalcase.presentation.splashScreen
 
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.yusuf.paparafinalcase.R
+import com.yusuf.paparafinalcase.core.constants.Constants.APP_NAME
+import com.yusuf.paparafinalcase.ui.theme.Orange
+import com.yusuf.paparafinalcase.ui.theme.White
 
 
 @Composable
 fun SplashScreen() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = "This is Splash Screen")
+    var startAnimation by remember { mutableStateOf(false) }
+
+    val rotation by animateFloatAsState(
+        targetValue = if (startAnimation) 360f else 0f,
+        animationSpec = infiniteRepeatable(animation = tween(durationMillis = 2000), repeatMode = RepeatMode.Restart),
+        label = ""
+    )
+
+    LaunchedEffect(Unit) {
+        startAnimation = true
     }
+
+    Column(Modifier.fillMaxSize().background(Orange), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_splash),
+            contentDescription = null,
+            modifier = Modifier
+                .size(100.dp)
+                .graphicsLayer(rotationZ = rotation)
+
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(text = APP_NAME)
+    }
+
 }
+
