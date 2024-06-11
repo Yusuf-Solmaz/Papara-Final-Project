855c05bee51c2e95264317e60e79dbccd6895714
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-11 14:02:30 +0300

    DailyRecommendation Class Added

--- app/src/main/java/com/yusuf/paparafinalcase/data/local/db/FoodDatabase.kt
+++ app/src/main/java/com/yusuf/paparafinalcase/data/local/db/FoodDatabase.kt
@@ -2,10 +2,13 @@ package com.yusuf.paparafinalcase.data.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.yusuf.paparafinalcase.data.local.dao.DailyRecommendationDao
 import com.yusuf.paparafinalcase.data.local.dao.FoodDao
+import com.yusuf.paparafinalcase.data.local.model.DailyRecommendation
 import com.yusuf.paparafinalcase.data.local.model.LocalFoods
 
-@Database(entities = [LocalFoods::class], version = 1, exportSchema = false)
+@Database(entities = [LocalFoods::class, DailyRecommendation::class], version = 1, exportSchema = false)
 abstract class FoodDatabase: RoomDatabase() {
     abstract fun foodDao(): FoodDao
+    abstract fun dailyRecommendationDao(): DailyRecommendationDao
 }
