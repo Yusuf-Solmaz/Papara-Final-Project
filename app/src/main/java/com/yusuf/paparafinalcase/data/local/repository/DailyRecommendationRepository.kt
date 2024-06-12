9ec824eab4519718e3f17b9ad3ea81df66a608b8
Yusuf Mücahit Solmaz <yusufmucahitsolmaz@gmail.com>
2024-06-12 04:12:20 +0300

    Notification Added

--- app/src/main/java/com/yusuf/paparafinalcase/data/local/repository/DailyRecommendationRepository.kt
+++ app/src/main/java/com/yusuf/paparafinalcase/data/local/repository/DailyRecommendationRepository.kt
@@ -1,16 +1,23 @@
 package com.yusuf.paparafinalcase.data.local.repository
 
-
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import com.yusuf.paparafinalcase.R
+import com.yusuf.paparafinalcase.core.constants.utils.notification.NotificationHelper.showNotification
 import com.yusuf.paparafinalcase.data.local.dao.DailyRecommendationDao
 import com.yusuf.paparafinalcase.data.local.model.DailyRecommendation
+import com.yusuf.paparafinalcase.data.mapper.toJsonString
+import com.yusuf.paparafinalcase.data.mapper.toLocalFoods
 import com.yusuf.paparafinalcase.data.remote.network.ApiService
-
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class DailyRecommendationRepository @Inject constructor(
+    private val context: Context,
     private val dailyRecommendationDao: DailyRecommendationDao,
     private val foodApi: ApiService
 ) {
@@ -29,6 +36,8 @@ class DailyRecommendationRepository @Inject constructor(
             )
             dailyRecommendationDao.clearDailyRecommendation()
             dailyRecommendationDao.insertDailyRecommendation(newRecommendation)
+            showNotification(context,"Daily Recommendation",
+                "Check out today's recommended food: ${newRecommendation.name}")
             dailyRecommendationDao.getDailyRecommendation()
         } else {
             dailyRecommendationDao.getDailyRecommendation()
